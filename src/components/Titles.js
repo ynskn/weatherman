33276d43d1fe8c44628dcc6fe20c03fc47eed5f9
ynskn <56791319+ynskn@users.noreply.github.com>
2019-10-24 13:05:13 +0900

    Add files via upload

---
+++ src/components/Titles.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Titles extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>It's the Weatherman!</h1>
+        <p>We're going to find out the temperature, conditions and more...</p>
+      </div>
+    );
+  }
+};
+export default Titles;
